f47ef6694e1d69d1f72178638efcd6687a9cd142
hp961203 <104044756+hp961203@users.noreply.github.com>
2024-04-01 13:32:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,8 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {"APP_ID":"wxa9c9a86fb5c2dc4a","APP_SECRET":"d491c318fb65e62a59559613f2b2dc0f","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"胡平","id":"oEz9V6X1Svk3LQscvTAdzC8FRS58","useTemplateId":"WIm51jnWwSRQPm8N7JxrmbX2aD7PYP07xcWyR1Fngc4","province":"天津","city":"天津","horoscopeDate":"01-11","horoscopeDateType":"今年","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"生日","name":"老婆生日","date":"07-03","year":"2001"},{"type":"生日","name":"老公生日","date":"01-11","year":"1997"}],"customizedDateList":[{"keyword":"一周年纪念日","date":"2024-04-15"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
-
-module.exports = USER_CONFIG
+const USER_CONFIG = {"APP_ID":"wxa9c9a86fb5c2dc4a","APP_SECRET":"d491c318fb65e62a59559613f2b2dc0f","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"李倍宁","id":"oEz9V6e5fMCTN_VjaCdPbbebUCaU","useTemplateId":"WIm51jnWwSRQPm8N7JxrmbX2aD7PYP07xcWyR1Fngc4","province":"天津","city":"天津","horoscopeDate":"07-03","horoscopeDateType":"今年","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"生日","name":"老公生日","date":"07-03","year":"2001"}],"customizedDateList":[{"keyword":"一周年纪念日","date":"2024-04-15"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
+  
+  
+  module.exports = USER_CONFIG
 
